7d35d75f41099c6e013a82a2460dd29f4b4890ab
dorrdorr9311 <dorrdorr9311@gmail.com>
2021-10-05 15:56:22 +0900

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
         test: /\.vue$/,
         use: 'vue-loader'
       },
